2a4ef313c20d9da15b04dbd890cfc0327d1f4598
Jordan Victor <jordan.scher@cobasi.com.br>
2023-03-23 20:08:15 -0300

    feat: add env config file; add start log

---
+++ src/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  NODE_ENV: process.env.NODE_ENV || 'dev',
+  NODE_PORT: Number(process.env.NODE_PORT) || 3000,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import env from './config/env.js';
+import { Logger } from '@nestjs/common';
+
+const portNumber = env.NODE_PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(portNumber);
+  Logger.log(`Application running on port ${portNumber} - ${env.NODE_ENV}`);
 }
+
 bootstrap();
