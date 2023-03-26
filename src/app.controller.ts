5bc8b55b8737672c185f24af4b78637f1604d403
Jordan Victor <jordan.scher@cobasi.com.br>
2023-03-26 17:50:39 -0300

    feat: redirect to /api from /

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @ApiOperation({ summary: 'Initial Page' })
+  @ApiResponse({ status: 200 })
+  @Get('/')
+  Redirect(@Res() res) {
+    return res.redirect('/api');
+  }
 
   @ApiOperation({ summary: 'Health check' })
   @ApiResponse({ status: 200, description: 'ok' })
