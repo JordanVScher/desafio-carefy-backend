8d48b14968d4ee6b33203e3d4ad94d54a74c89c9
Jordan Victor <jordan.scher@cobasi.com.br>
2023-03-26 13:48:06 -0300

    feat: add cors

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,4 +2,5 @@ export default {
   NODE_ENV: process.env.NODE_ENV || 'dev',
   NODE_PORT: Number(process.env.NODE_PORT) || 3000,
   MONGO_URL: process.env.MONGO_URL,
+  CORS_ALLOWED_ORIGINS_REGEX: process.env.CORS_ALLOWED_ORIGINS_REGEX,
 };
--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,31 @@ import env from './config/env.js';
 import { Logger, ValidationPipe } from '@nestjs/common';
 
 const portNumber = env.NODE_PORT;
+const corsAllowedOrigins = env.CORS_ALLOWED_ORIGINS_REGEX;
+
+const headersList = [
+  'Content-Type',
+  'checkoutDataAccess',
+  'authorization',
+  'Access-Control-Allow-Origin',
+  'Connection',
+  'Origin',
+  'user-agent',
+  'X-Cobasi-Session',
+  'X-Cobasi-Email',
+  'Set-Cookie',
+];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: new RegExp(corsAllowedOrigins),
+    allowedHeaders: headersList,
+    exposedHeaders: headersList,
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(portNumber);
   Logger.log(`Application running on port ${portNumber} - ${env.NODE_ENV}`);
