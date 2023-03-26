fa6aff9a5dcf2120fe85de55af75d9af96d02f07
Jordan Victor <jordan.scher@cobasi.com.br>
2023-03-26 17:27:13 -0300

    chore: add swagger basic config

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import env from './config/env.js';
 import { Logger, ValidationPipe } from '@nestjs/common';
@@ -24,6 +25,13 @@ const headersList = [
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('carefy-desafio-backend')
+    .setDescription('Backend API for carefy-desafio')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   app.enableCors({
     origin: new RegExp(corsAllowedOrigins),
